02e941f5d8c6b1629f57c2ed9154db14256e4a07
Protmir <kkazak007@gmail.com>
2021-03-11 01:31:38 +0300

    add API part in application

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,11 +1,39 @@
 import {useRouter} from 'next/router';
 import {MainLayout} from '../../components/MainLayout';
 
-export default function Post() {
+
+export default function Post({ post }) {
     const router = useRouter()
+    // console.log(router.query.id)
+    // async function getInitialProps() {
+    //     const response = await fetch(`http://localhost:4200/posts/${router.query.id}`)
+    //     const post = await response.json()
+    //     console.log(post)
+    //
+    //     return {
+    //         post
+    //     }
+    // }
+
+
     return (
     <MainLayout>
-        <h1>Post {router.query.id}</h1>
+        {post.map(post => (
+            <div key={post.id}>
+                <h1>{post.title}</h1>
+                <p>{post.body}</p>
+            </div>
+        ))}
     </MainLayout>
     )
+}
+
+Post.getInitialProps = async (pathname) => {
+    console.log(pathname)
+    const response = await fetch(`http://localhost:4200/posts`)
+    const post = await response.json()
+
+    return {
+        post
+    }
 }
--- pages/posts.js
+++ pages/posts.js
@@ -1,13 +1,45 @@
+import {useState, useEffect} from 'react'
 import Head from 'next/head';
 import {MainLayout} from '../components/MainLayout';
+import Link from 'next/link';
 
-export default function Posts() {
+export default function Posts({ posts }) {
+    // const [posts, setPosts] = useState([])
+    
+    // useEffect(() => {
+    //     async function load() {
+    //         const response = await fetch('http://localhost:4200/posts')
+    //         const json = await response.json()
+    //         setPosts(json)
+    //     }
+    //
+    //     load()
+    // }, [])
+    
     return (
         <MainLayout title={'Posts Page'}>
             <Head>
                 <title>Posts page | Next Learning</title>
             </Head>
             <h1>Posts page</h1>
+            <ul>
+                {posts.map(post => (
+                  <li key={post.id}>
+                      <Link href={`/post/[id]`} as={`/post/${post.id}`}>
+                          <a>{post.title}</a>
+                      </Link>
+                  </li>
+                ))}
+            </ul>
         </MainLayout>
     )
+}
+
+Posts.getInitialProps = async () => {
+    const response = await fetch('http://localhost:4200/posts')
+    const posts = await response.json()
+
+    return {
+        posts
+    }
 }
